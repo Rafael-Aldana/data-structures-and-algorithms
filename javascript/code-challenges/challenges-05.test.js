fd1e7c65c9f435a67c5bd853ef3dcace30df22b9
Rafael Aldana <rafaelaldana6@Gmail.com>
2023-01-15 23:13:37 -0800

    finished challenges

--- javascript/code-challenges/challenges-05.test.js
+++ javascript/code-challenges/challenges-05.test.js
@@ -3,34 +3,28 @@
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
 
-Write a function that iterates over an array of people objects
-and creates a new list of each person's full name using the array method 'map'.
-Each object will have the shape {firstName:string, lastName:string}
+//Write a function that iterates over an array of people objects//
+and //creates a new list of each person's full name using the array method 'map'.//
+//Each object will have the shape {firstName:string, lastName:string}
 E.g. [ { firstName:"Jane", lastName:"Doe" }, { firstName:"James", lastName:"Bond"}]
-should convert to ["Jane Doe", "James Bond"]
+should convert to ["Jane Doe", "James Bond"]//
 Note the space in between first and last names.
 You can assume that neither firstName nor lastName will be blank
 ------------------------------------------------------------------------------------------------ */
-const toLastNames = people => {
-  // Solution code here...
-};
+const toLastNames = people => people.map(person => `${person.firstName}  ${person.lastName}`);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
-Write a function named addValues that, given an array of numbers as input, uses reduce to add the values in the array.
+//Write a function named addValues// that, //given an array of numbers as input,// //uses reduce to add the values in the array.//
 
 ------------------------------------------------------------------------------------------------ */
 
-const addValues = (arr) => {
-  .reduce(())
-  return()
-};
-
+const addValues = (arr) => arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function named addPurchases that, given an array of objects as input, uses reduce to find the total amount purchased. Each object contains the keys `item` and `purchasePrice` like the example.
+///Write a function named addPurchases// that, given an //array of objects as input, uses reduce// to //find the total amount purchased.// Each object contains the keys `item` and `purchasePrice` like the example.
 
 {
   item: 'switch'
@@ -39,26 +33,24 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 ------------------------------------------------------------------------------------------------ */
 
-const addPurchases = (arr) => arr.reduce((acc, curr) => {
-  acc = acc + curr.purchasePrice;
-  return acc;
+const addPurchases = (arr) => arr.reduce((accumulator, currentValue) => {
+  accumulator = accumulator + currentValue.purchasePrice;
+  return accumulator;
 } , 0);
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
 
-Write a function named countNumberOfElements that, given an array as input, uses reduce to count the number of elements in the array.
+//Write a function named countNumberOfElements// that, given //an array as input, uses reduce to count the number of elements in the array.//
 
 Note: You may not use the array's built-in length property.
 ------------------------------------------------------------------------------------------------ */
 
-const countNumberOfElements = (arr) => {
-  // Solution code here...
-};
+const countNumberOfElements = (arr) => arr.reduce((accumulator, currentValue) => accumulator + 1, 0);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
-Write a function named returnNames that, given the Star Wars data, below, uses reduce to return an array containing the names of the characters.
+//Write a function named returnNames// that, //given the Star Wars data, below, uses reduce to return an array containing the names of the characters.//
 ------------------------------------------------------------------------------------------------ */
 
 let starWarsData = [{
@@ -111,20 +103,21 @@ let starWarsData = [{
   gender: 'female'
 }];
 
-const returnNames = (arr) => {
-  // Solution code here...
-};
+const returnNames = (arr) => arr.reduce((accumulator, nameData) => {
+  accumulator.push(nameData.name)
+  return accumulator;
+} ,[]);
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
 
-Write a function named reversedString that takes in a string and returns a string with the letters in reverse order.
+//Write a function named reversedString// that //takes in a string and returns a string with the letters in reverse order.//
 
 Note: You must use reduce for this challenge. You may not use the built-in .reverse() string method.
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (str) => {
-  // Solution code here...
+  
 };
 
 /* ------------------------------------------------------------------------------------------------
